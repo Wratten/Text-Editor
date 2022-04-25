6d857a0337ee7d1456b3a69f42f69e02ac8a81e8
Wratten <daniel.wratten@gmail.com>
2022-04-25 16:18:10 +0800

    added indexdb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,25 +1,57 @@
-// TODO: Install the following package:
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
-// TODO: Complete the initDb() function below:
-const initdb = async () => {
+const initdb = async () =>
+  openDB("jate", 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
+        return;
+      }
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
+    },
+  });
 
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  // Create a connection to the database database and version we want to use.
+  const jate = await openDB("jate", 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jate.transaction("jate", "readwrite");
+
+  // Open up the desired object store.
+  const store = tx.objectStore("jate");
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.add({ key: 1, content: content });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
 };
 
-
-// TODO: Complete the postDb() function below:
-export const postDb = async (name, home, cell, email)  => {
- 
-};
-
-// TODO: Complete the getDb() function below:
+// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  
-};
+  console.log("GET all from the database");
 
-// TODO: Complete the deleteDb() function below:
-export const deleteDb = async (id) => {
-  
+  // Create a connection to the database database and version we want to use.
+  const jate = await openDB("jate", 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jate.transaction("jate", "readonly");
+
+  // Open up the desired object store.
+  const store = tx.objectStore("jate");
+
+  // Use the .getAll() method to get all data in the database.
+  const id = 1;
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("result.value", result);
+  return result;
 };
 
 initdb();
